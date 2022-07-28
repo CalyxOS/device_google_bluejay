CalyxOS/device_google_crosshatch

Setup EUICC permissions
Includes:
commit 1edcab72d1c49402267e427a3dd153404dfcdeda
Author: Ethan Chen <intervigil@gmail.com>
Date:   Sat Dec 1 13:53:00 2018 -0800

    crosshatch: Declare EUICC permission

    Change-Id: I1b151021ca9fc6cf35141b2ccb337bc83197089e
    (cherry picked from commit 5fa85cb19a3fdd17f1a6cfb301524cf9bf9481d3)

commit e8c0ef356137d30bad8003f37f4a9e11022faeb6
Author: Rashed Abdel-Tawab <rashed@linux.com>
Date:   Wed May 22 15:13:23 2019 -0700

    crosshatch: Add privapp permissions for Google EUICC apps

    Change-Id: I2e7bc09bac103d31132bd6ae646cb2f6d7ba8b19
    (cherry picked from commit e981817440c5e5e90f5aa80e95918a167c27165a)

commit daa003e9390468b435ab508492bc336552fbe63e
Author: Chirayu Desai <chirayudesai1@gmail.com>
Date:   Tue Jun 8 22:22:22 2021 +0530

    Refresh privapp-permissions for the Euicc apps

    Change-Id: I5b1007425a94a9ccc8c01c04b3db697d49a13a84
    (cherry picked from commit a1a7505168e5501e9be5c61adb4fae0ba1c35f4e)

commit ce0c38b061b10047e46324af63e59c511c7c789f
Author: Michael Bestas <mkbestas@gmail.com>
Date:   Mon Jun 14 15:28:21 2021 +0300

    Move euicc permission to product partition

    Change-Id: Iecd09978aa62e74bf82fc18aa915b6ece715b56f
    (cherry picked from commit 793fa232deb0bf03194f49f3340cd17a6f24d306)

commit eb8b99fc321d6c1d9e56720b25fe58ebfdbf5e3f
Author: Chirayu Desai <chirayudesai1@gmail.com>
Date:   Thu Oct 28 05:32:22 2021 +0530

    Refresh EUICC perms for 12

    Change-Id: Ib247ed64b589226d17bf7e9fc762c1e30e63960c

Change-Id: I093cd52996b11b88343c474e85d34595b10f5c62

device-calyx.mk
@@ -1,1 +1,7 @@
 DEVICE_PACKAGE_OVERLAYS += device/google/crosshatch/overlay-calyx
+
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
+    device/google/crosshatch/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml \
+    device/google/crosshatch/permissions/permissions_com.google.euiccpixel.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.google.euiccpixel.xml

CalyxOS/device_google_bluejay

device-calyx.mk
@@ -1,1 +1,7 @@
 DEVICE_PACKAGE_OVERLAYS += device/google/bluejay/overlay-calyx
+
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
+    device/google/bluejay/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml \
+    device/google/bluejay/permissions/permissions_com.google.euiccpixel.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.google.euiccpixel.xml