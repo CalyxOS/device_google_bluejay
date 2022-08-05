5af6a005f149af71aa813a89d667835a42599fab
Chirayu Desai <chirayudesai1@gmail.com>
2022-08-04 20:11:42 -0400

    Update fingerprint/build description from SD2A.220601.003
    Change-Id: I6eb5290e5a3d3cc5692af21347579ee725ec60cc

--- calyx_bluejay.mk
+++ calyx_bluejay.mk
@@ -13,9 +13,9 @@ PRODUCT_BRAND := google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=bluejay \
-    PRIVATE_BUILD_DESC="bluejay-user 12 SD2A.220601.004 8804960 release-keys"
+    PRIVATE_BUILD_DESC="bluejay-user 12 SD2A.220601.003 8804744 release-keys"
 
-BUILD_FINGERPRINT := google/bluejay/bluejay:12/SD2A.220601.004/8804960:user/release-keys
+BUILD_FINGERPRINT := google/bluejay/bluejay:12/SD2A.220601.003/8804744:user/release-keys
 
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
