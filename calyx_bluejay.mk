cf237425f137ec81f9612453978581549ea9e577
Michael Bestas <mkbestas@gmail.com>
2024-10-25 21:55:33 +0300

    Adapt to new build prop override scheme again
    Change-Id: I8faf333d7a4b7efd95cdf0e2b5a7d504b1f5bd57

--- calyx_bluejay.mk
+++ calyx_bluejay.mk
@@ -22,7 +22,7 @@ PRODUCT_MODEL := Pixel 6a
 PRODUCT_NAME := calyx_$(DEVICE_CODENAME)
 
 PRODUCT_BUILD_PROP_OVERRIDES := \
-    BuildDesc=bluejay-user[[:space:]]15[[:space:]]AP3A.241005.015[[:space:]]12366759[[:space:]]release-keys \
+    BuildDesc="bluejay-user 15 AP3A.241005.015 12366759 release-keys" \
     BuildFingerprint=google/bluejay/bluejay:15/AP3A.241005.015/12366759:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
